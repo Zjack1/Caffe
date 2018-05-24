89c89aede24527692c488e96001a4e16ff7e01da
Jack <37362585+Zjack1@users.noreply.github.com>
2018-05-24 08:01:56 -0500

    Update create_filelist.sh

--- Train_my_img/create_filelist.sh
+++ Train_my_img/create_filelist.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env sh
-    DATA=data/re/
-    MY=examples/myfile
+    DATA=/home/lin/caffe/data/re/
+    MY=/home/lin/caffe/examples/myfile
 
     echo "Create train.txt..."
     rm -rf $MY/train.txt
     for i in 3 4 5 6 7 
     do
-    find $DATA/train -name $i*.jpg | cut -d '/' -f4-5 | sed "s/$/ $i/">>$MY/train.txt
+    find $DATA/train -name $i*.jpg | cut -d '/' -f8-9 | sed "s/$/ $i/">>$MY/train.txt
     done
     echo "Create test.txt..."
     rm -rf $MY/test.txt
     for i in 3 4 5 6 7
     do
-    find $DATA/test -name $i*.jpg | cut -d '/' -f4-5 | sed "s/$/ $i/">>$MY/test.txt
+    find $DATA/test -name $i*.jpg | cut -d '/' -f8-9 | sed "s/$/ $i/">>$MY/test.txt
     done
     echo "All done"
     
