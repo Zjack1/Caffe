871abe2aa688b451a25e3c951f0f6a8c76f0f83c
Jack <37362585+Zjack1@users.noreply.github.com>
2018-05-24 08:01:13 -0500

    Update create_lmdb.sh

--- Train_my_img/create_lmdb.sh
+++ Train_my_img/create_lmdb.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
-    MY=examples/myfile
+    MY=/home/lin/caffe/examples/myfile
 
 
     echo "Create train lmdb.."
     rm -rf $MY/img_train_lmdb
-    build/tools/convert_imageset \
+    /home/lin/caffe/build/tools/convert_imageset \
     --shuffle \
     --resize_height=256 \
     --resize_width=256 \
-    /home/xxx/caffe/data/re/ \
+    /home/lin/caffe/data/re/ \
     $MY/train.txt \
     $MY/img_train_lmdb
 
 
     echo "Create test lmdb.."
     rm -rf $MY/img_test_lmdb
-    build/tools/convert_imageset \
+    /home/lin/caffe/build/tools/convert_imageset \
     --shuffle \
     --resize_width=256 \
     --resize_height=256 \
-    /home/xxx/caffe/data/re/ \
+    /home/lin/caffe/data/re/ \
     $MY/test.txt \
     $MY/img_test_lmdb
 
