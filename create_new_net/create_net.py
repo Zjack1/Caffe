bc6297939f8862a57fd518b9398e241eee0abd3a
Jack <37362585+Zjack1@users.noreply.github.com>
2018-05-25 10:23:34 +0800

    Create create_net.py

---
+++ create_new_net/create_net.py
@@ -0,0 +1,24 @@
+import sys
+caffe_root='/home/zj/caffe/'
+sys.path.insert(0,caffe_root+'python')
+from caffe import layers as L
+from caffe import params as P
+import caffe
+
+def lenet(lmdb,batch_size):
+	n=caffe.NetSpec()
+	n.data,n.label=L.Data(batch_size=batch_size,backend=P.Data.LMDB,source=lmdb,transform_param=dict(scale=1./255),ntop=2)
+	n.conv1=L.Convolution(n.data,kernel_size=5,num_output=20,weight_filler=dict(type='xavier'))
+	n.pool1=L.Pooling(n.conv1,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+	n.conv2=L.Convolution(n.pool1,kernel_size=5,num_output=50,weight_filler=dict(type='xavier'))
+	n.pool2=L.Pooling(n.conv2,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+	n.ip1=L.InnerProduct(n.pool2,num_output=500,weight_filler=dict(type='xavier'))
+	n.relu1=L.ReLU(n.ip1,in_place=Ture)
+	n.ip2=L.InnerProduct(n.relu1,num_output=10,weight_filler=dict(type='xavier'))
+	n.loss=L.SoftmaxWithLoss(n.ip2,n.label)
+	return n.to_proto()
+
+with open('lenet_auto_train.prototxt','w') as f:
+	f.write(str(lenet('/home/zj/caffe/examples/my/my_train_lmdb',32)))
+with open('lenet_auto_test.prototxt','w') as f:
+	f.write(str(lenet('/home/zj/caffe/examples/my/my_test_lmdb',32)))
