399c19b7e9ebbd1b6b1264ac5a7733171161a806
Jack <37362585+Zjack1@users.noreply.github.com>
2018-06-05 09:25:03 +0800

    Create 生成均值.bat

---
+++ caffe_for_windows_GPU/生成均值.bat
@@ -0,0 +1,3 @@
+D:/caffe/Build/x64/Release/compute_image_mean.exe --backend=leveldb E:/caffe/caffe/examples/myfile/ilsvrc12_train_lmdb E:/caffe/caffe/examples/myfile/ilsvrc12_train_lmdb/mean.binaryproto 
+D:/caffe/Build/x64/Release/compute_image_mean.exe --backend=leveldb E:/caffe/caffe/examples/myfile/ilsvrc12_val_lmdb E:/caffe/caffe/examples/myfile/ilsvrc12_val_lmdb/mean.binaryproto 
+pause 
